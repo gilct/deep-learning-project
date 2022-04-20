969e43bbe88831b613607e058956223f60d83df5
Gil Tinde <giltinde@hotmail.com>
2022-04-20 15:56:59 +0200

    Add comments to test script

--- Miniproject_2/test.py
+++ Miniproject_2/test.py
@@ -21,15 +21,20 @@ class LinearTorchTest(nn.Module):
 
 class Testing(unittest.TestCase):
     def test_linear(self):
+        """Compare our implementation with baseline torch model."""
 
-        in_dim, hidden_dim, out_dim = 5, 3, 1
+        # Define dimensions of data and linear layers (1 input, 1 hidden, 1 output)
+        in_dim, hidden_dim, out_dim = 50, 20, 15
         n_samples = 200
+
+        # Parameters of distribution of inputs and targets
         mean, std = 0, 20
         unif_lower, unif_upper = 10, 15
         train_input = torch.empty(n_samples, in_dim).normal_(mean, std)
         train_targets = torch.empty(n_samples, out_dim).uniform_(unif_lower,unif_upper)
-        init_val = 0.05
 
+        # Define our model and torch model with same initial value of all weights (for reproducibility)
+        init_val = 0.05
         model_no_torch = Sequential(Linear(in_dim, hidden_dim, init_val=init_val),
                                     Sigmoid(),
                                     Linear(hidden_dim, out_dim, init_val=init_val))
@@ -41,17 +46,20 @@ class Testing(unittest.TestCase):
             model_torch.fc2.weight = nn.Parameter(torch.full_like(model_torch.fc2.weight, init_val))
             model_torch.fc2.bias = nn.Parameter(torch.full_like(model_torch.fc2.bias, init_val))
 
+        # Training parameters and variables
         lr, nb_epochs, batch_size = 1e-1, 1, 20
 
         optimizer_no_torch = SGD(model_no_torch.param(), lr=lr)
         criterion_no_torch = MSE()
 
-        optimizer_torch = torch.optim.SGD(model_torch.parameters(), lr = lr)
+        optimizer_torch = torch.optim.SGD(model_torch.parameters(), lr=lr)
         criterion_torch = nn.MSELoss()
 
+        # Standardize data
         mu, std = train_input.mean(), train_input.std()
         train_input.sub_(mu).div_(std)
 
+        # Train
         for e in range(nb_epochs):
             for input, targets in zip(train_input.split(batch_size),
                                       train_targets.split(batch_size)):
@@ -59,11 +67,15 @@ class Testing(unittest.TestCase):
                 output_no_torch = model_no_torch.forward(input)
                 output_torch = model_torch(input)
 
+                # Retain grad for comparing MSE gradients, see below
+                # output_torch.retain_grad()
+
+                # Store statistics of the output for comparison
                 stats_no_torch = (output_no_torch.mean().item(), output_no_torch.std().item())
                 stats_torch = (output_torch.mean().item(), output_torch.std().item())
 
                 loss_no_torch = criterion_no_torch.forward(output_no_torch, targets)
-                loss_torch = criterion_torch(output_torch, targets)                
+                loss_torch = criterion_torch(output_torch, targets)   
 
                 optimizer_no_torch.zero_grad()
                 optimizer_torch.zero_grad() 
@@ -71,11 +83,16 @@ class Testing(unittest.TestCase):
                 model_no_torch.backward(criterion_no_torch.backward())
                 loss_torch.backward()
 
+                # Compare gradients of MSE (make sure .retain_grad() is called on output_torch)
+                # print(criterion_no_torch.backward())
+                # print(output_torch.grad)
+
                 optimizer_no_torch.step()
                 optimizer_torch.step()
 
-                self.assertAlmostEqual(loss_no_torch.item(), loss_torch.item(), places=6, msg="Equal losses")
-                self.assertAlmostEqual(stats_no_torch[0], stats_torch[0], places=6, msg="Equal mean of preds")
+                # Assertions
+                self.assertAlmostEqual(loss_no_torch.item(), loss_torch.item(), places=5, msg="Equal losses")
+                self.assertAlmostEqual(stats_no_torch[0], stats_torch[0], places=5, msg="Equal mean of preds")
                 self.assertAlmostEqual(stats_no_torch[1], stats_torch[1], places=6, msg="Equal std of preds")
 
 if __name__ == '__main__':
